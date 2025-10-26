a0461101d5ea3c1f3393882e081b2baedb62c5f5
Aalok Pal <palaalok05@gmail.com>
2025-10-26 16:57:48 +0530

    Time: 99 ms (46.86%) | Memory: 101.4 MB (47.33%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.java
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -1,6 +1,6 @@
 class Bank {
-    private long[] bal;
-    private int n;
+    long[] bal;
+    int n;
     public Bank(long[] bal) {
         this.bal = bal;
         this.n = bal.length;
@@ -8,27 +8,24 @@ class Bank {
     
     public boolean transfer(int account1, int account2, long money) {
         if (account1 > n || account2 > n || bal[account1-1] < money) return false;
-        else {
+
         bal[account1-1] -= money;
         bal[account2-1] += money;
         return true;
-        }
     }
     
     public boolean deposit(int account, long money) {
         if (account > n) return false;
-        else {
-            bal[account-1] += money;
-            return true;
-        }
+
+        bal[account-1] += money;
+        return true;
     }
     
     public boolean withdraw(int account, long money) {
         if (account > n || bal[account-1] < money) return false;
-        else {
-            bal[account-1] -= money;
-            return true;
-        }
+
+        bal[account-1] -= money;
+        return true;
     }
 }
 
