80bfe2ef492f5d2a2a14508d4d72a87544d74930
Aalok Pal <palaalok05@gmail.com>
2025-10-28 21:10:24 +0530

    Time: 1 ms (83.13%) | Memory: 42.1 MB (80.12%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,39 +1,20 @@
 class Solution {
-
-    public boolean check(int[] nums,int c,int i,int dir){
-        int idx = i,n = nums.length ;
-
-        int[] temp = Arrays.copyOf(nums,nums.length);
-
-        while(idx>=0 && idx<temp.length && c>0){
-
-            if(temp[idx]>0){
-
-                temp[idx]--;
-                dir *= -1;// dir change hulaboo....hu .. hu hu .... 
-
-                if(temp[idx]==0) c--;
-            }
-
-            idx += dir;
-        }
-        return c==0;// all must be zero
-    }
-
     public int countValidSelections(int[] nums) {
-        int res = 0;
-        int n = nums.length;
+        int n = nums.length,res = 0;
+        int S = 0;
 
-        int c = 0;// count of non zero ele
-        for(int i=0;i<n;i++) if(nums[i]!=0) c++;
+        for(int ele : nums) S += ele;
 
+        int  currSum = 0;
         for(int i=0;i<n;i++){
+            currSum += nums[i];
 
-            if(nums[i]==0){
+            int l = currSum;
+            int r = S - currSum;
 
-                if(check(nums,c,i,-1)) res++;// -1 for left direction
-                if(check(nums,c,i,1)) res++;// +1 for right direction;
-            }
+            if(nums[i]!=0) continue;
+            if(l==r) res += 2;
+            else if(Math.abs(l-r)==1) res++;
         }
         return res;
     }
