6d79e7106d57c416374a3a7f6797e14101157970
Aalok Pal <palaalok05@gmail.com>
2025-10-28 12:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (40.80%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int stair(int n,int[] dp){
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = stair(n-1,dp)+stair(n-2,dp);
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return stair(n,dp);
+    }
+}
