4f9ed913a7510b28ebb9ebee8ff53ca495fd1d1c
Aalok Pal <palaalok05@gmail.com>
2025-10-29 11:23:46 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (49.51%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,16 +1,13 @@
 class Solution {
-    public int ways(int m,int n,int[][] dp){
-        if(m<0 || n<0) return 0;
-        if(m==0 || n==0) return 1;
-        if(dp[m][n]!=-1) return dp[m][n];
-        return dp[m][n] = ways(m-1,n,dp)+ways(m,n-1,dp);
-    }
     public int uniquePaths(int m, int n) {
         // row 0 to m-1   |   col 0 to n-1
         int[][] dp = new int[m][n];
-        for (int[] row : dp) {
-        Arrays.fill(row,-1);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
         }
-        return ways(m-1,n-1,dp);
+        return dp[m-1][n-1];
     }
 }
