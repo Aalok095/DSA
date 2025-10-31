7f542ab70c6675cccea42ea41722c74bf110dc3c
Aalok Pal <palaalok05@gmail.com>
2025-11-01 00:38:35 +0530

    Time: 24 ms (81.69%) | Memory: 44.6 MB (21.58%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -3,19 +3,43 @@ class Solution {
         int sqrt = (int)Math.sqrt(n);
         return sqrt*sqrt==n;
     }
-    public int minSquares(int n,int[] dp) {
-        if(isPerfect(n)) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int min = n;// 96 ka ans 96 ho sakta hai max 1..1...1......1....1... * 96 times
-        for(int i=1;i*i<=n;i++){
-            int count = minSquares(i*i,dp) + minSquares(n-i*i,dp);
-            min = Math.min(min,count);
-        }
-        return dp[n] = min;
-    }
     public int numSquares(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return minSquares(n,dp);
+        for(int i=1;i<=n;i++){
+            if(isPerfect(i)) dp[i] = 1;
+            else {
+                int min = i;
+                for(int j=1;j*j<=i;j++){
+                    int count = dp[j*j] + dp[i-j*j];
+                    min = Math.min(min,count);
+                }
+                dp[i] = min;
+            }
+        }
+        return dp[n];
     }
 }
+
+// rec + memo - T.C n*root n
+
+// class Solution {
+//     public boolean isPerfect(int n){
+//         int sqrt = (int)Math.sqrt(n);
+//         return sqrt*sqrt==n;
+//     }
+//     public int minSquares(int n,int[] dp) {
+//         if(isPerfect(n)) return 1;
+//         if(dp[n]!=-1) return dp[n];
+//         int min = n;// 96 ka ans 96 ho sakta hai max 1..1...1......1....1... * 96 times
+//         for(int i=1;i*i<=n;i++){
+//             int count = minSquares(i*i,dp) + minSquares(n-i*i,dp);
+//             min = Math.min(min,count);
+//         }
+//         return dp[n] = min;
+//     }
+//     public int numSquares(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return minSquares(n,dp);
+//     }
+// }
