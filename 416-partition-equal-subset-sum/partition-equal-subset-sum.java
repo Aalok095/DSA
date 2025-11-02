96e326ae5eb8809837d5ee1f30881cd1bce16bb1
Aalok Pal <palaalok05@gmail.com>
2025-11-02 23:37:31 +0530

    Time: 78 ms (32.13%) | Memory: 72.4 MB (7.91%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,26 +1,24 @@
 class Solution {
-    public boolean subset(int i,int[] arr,int target,int[][] dp){
-        if(i==arr.length) return target==0;
-        if(dp[i][target]!=-1) return dp[i][target]==1;
-        boolean ans = false;
-        boolean skip = subset(i+1,arr,target,dp);
-        if(target-arr[i]<0) ans = skip;
-        else {
-            boolean pick = subset(i+1,arr,target-arr[i],dp);
-            ans = skip || pick;
-        }
-        dp[i][target] = ans ? 1 : 0;
-        return ans;
-    }
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+        int sum = 0,n = nums.length;
         for(int ele : nums) sum+=ele;
         if(sum%2!=0) return false;
 
         int target = sum/2;
         //i -> 0 to n-1 | target -> target to 0
-        int[][] dp = new int[nums.length][target+1];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return subset(0,nums,target,dp);
+        int[][] dp = new int[n][target+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<target+1;j++){
+                boolean ans = false;
+                boolean skip = i>0 ? dp[i-1][j]==1 : j==0;//j->target;
+                if(j-nums[i]<0) ans = skip;
+                else {
+                    boolean pick = i>0 ? dp[i-1][j-nums[i]]==1 : j==0;
+                    ans = pick || skip;
+                }
+                dp[i][j] = ans ? 1 : 0;
+            }
+        }
+        return dp[n-1][target]==1;
     }
 }
