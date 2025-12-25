61e3f417f824c2e2051fdf2d8bd3889f2c96bd28
Aalok Pal <palaalok05@gmail.com>
2025-12-25 15:52:24 +0530

    Time: 1 ms (83.07%) | Memory: 42.9 MB (8.58%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPerfectSquare(int n){
+        int sqrt = (int)Math.sqrt(n);
+        return sqrt*sqrt==n;
+    }
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+        return ((n & n-1)==0 && isPerfectSquare(n));
+    }
+}
