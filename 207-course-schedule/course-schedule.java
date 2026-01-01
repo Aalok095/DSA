9c461179e853e8ea53ac81b9ece85ca95f282533
Aalok Pal <palaalok05@gmail.com>
2026-01-01 23:56:08 +0530

    Time: 8 ms (32.48%) | Memory: 47.4 MB (14.69%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prereq) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
+        int[] in = new int[n]; // indegree array
+        boolean[] vis = new boolean[n];
+
+        for(int[] p : prereq){
+            int a = p[0],b = p[1]; // b -> a edge ja rahi hai
+            adj.get(b).add(a);
+            in[a]++;// populating indegree too
+        }
+
+        // kahn's algorithm
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(in[i]==0){
+                q.add(i);
+                vis[i] = true;
+            } 
+        }
+        while(q.size()>0){
+            int front = q.poll();
+            ans.add(front);
+            for(int ele : adj.get(front)){
+                in[ele]--;
+                if(in[ele]==0){
+                    q.add(ele);
+                    vis[ele] = true;
+                }   
+            }
+        }
+        return ans.size()==n;
+    }
+}
