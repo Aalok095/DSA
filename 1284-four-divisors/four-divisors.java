274eeea85d445dfb0ea94dff036fb58c2f50fb35
Aalok Pal <palaalok05@gmail.com>
2026-01-04 12:00:05 +0530

    Time: 8 ms (98.74%) | Memory: 46.6 MB (33.08%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,23 +1,27 @@
 class Solution {
+    public boolean isPrime(int x){
+        if(x<2) return false;
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0) return false;
+        }
+        return true;
+    }
     public int sumFourDivisors(int[] nums) {
         int ans = 0;
         for(int ele : nums){
-            int s = ele+1;
-            int n = 2;
-            for(int i=2;i*i<=ele;i++){
-                if(ele%i==0){
-                    int d = ele/i;
-                    if(i==d){
-                        n++;
-                        s += i;
-                    }
-                    else {
-                        n += 2;
-                        s += d+i;
-                    }
-                }
+            int p = (int)Math.round(Math.cbrt(ele)); // ele = p^3
+            if((long)p*p*p==ele && isPrime(p)) {
+                ans += 1+p+p*p+ele;
+                continue;
             }
-            if(n==4) ans += s;
+            
+            for(int i=2;i*i<=ele;i++){// ele = p * q;
+                if(ele%i==0){
+                    int q = ele/i;
+                    if(i!=q && isPrime(i) && isPrime(q)) ans += 1+i+q+ele; 
+                    break;
+                }
+            }    
         }
         return ans;
     }
