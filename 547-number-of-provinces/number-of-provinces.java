0e92bdf45e945f9332cb91d2c269247fac50bde1
Aalok Pal <palaalok05@gmail.com>
2026-01-28 23:16:20 +0530

    Time: 2 ms (47.66%) | Memory: 47.3 MB (54.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,28 +1,36 @@
 class Solution {
-    public void bfs(int i,int[][] adj,boolean[] vis){
-        int n = adj.length;
-        vis[i] = true;
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(i);
-        while(!q.isEmpty()){
-            int temp = q.poll();
-            for(int j=0;j<n;j++){
-                if(adj[temp][j]==1 && !vis[j]){
-                    q.add(j);
-                    vis[j] = true;
-                }
-            }
-        }
+    static int[] parent;
+
+    public int find(int a){
+        if(parent[a] == a) return a;
+        return find(parent[a]);
     }
-    public int findCircleNum(int[][] adj) {
-        int n = adj.length, c = 0;
-        boolean[] vis = new boolean[n];
+
+    public void union(int a,int b){
+        int leaderA = find(a);
+        int leaderB = find(b);
+        // if leadA == leadB then there is a cycle so do nothing
+        if(leaderA != leaderB) {
+            parent[leaderB] = leaderA;
+        } 
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+
+        parent = new int[n+1]; // 1 based indexing;
+        for(int i=1;i<=n;i++) parent[i] = i;
+        parent[0] = 69;
+
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                bfs(i,adj,vis);
-                c++;
+            for(int j=0;j<n;j++){
+                // edge is from i+1 to j+1
+                if(i!=j && isConnected[i][j]==1)union(i+1,j+1); // conntecting group leaders of i+1 and j+1;
             }
         }
+        
+        int c = 0;
+        for(int i=1;i<=n;i++) if(parent[i]==i) c++;
         return c;
     }
 }
