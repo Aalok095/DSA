a4d8ddb09f5bd541c81eb19b89018c854bf0fa05
Aalok Pal <palaalok05@gmail.com>
2026-02-20 23:45:02 +0530

    Time: 2 ms (100.00%) | Memory: 43.3 MB (48.34%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int start = 0;
+        int sum = 0;
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            sum += (s.charAt(i)=='1') ? 1 : -1;
+            if(sum==0){
+                String inner = s.substring(start+1,i);
+                String processed = "1"+makeLargestSpecial(inner)+"0";
+                list.add(processed);
+                start = i+1;
+            }
+        }
+        Collections.sort(list,Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for(String ele : list) sb.append(ele);
+        return sb.toString();
+    }
+}
