1c9552bf1f0d37c11d1d365bbfd52dc17ed3704b
Aalok Pal <palaalok05@gmail.com>
2026-04-05 15:28:18 +0530

    Time: 4 ms (99.05%) | Memory: 46.2 MB (34.70%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -3,9 +3,9 @@ class Solution{
         int u = 0,l = 0;
         for(char ch : m.toCharArray()){
             if(ch=='L')l++;
-            if(ch=='R')l--;
-            if(ch=='U')u++;
-            if(ch=='D')u--;
+            else if(ch=='R')l--;
+            else if(ch=='U')u++;
+            else if(ch=='D')u--;
         }
         return l==0 && u==0;
     }
