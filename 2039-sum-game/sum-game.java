3db4f5b3ea4489ba6b098012f4e21359b84408ae
Aalok Pal <palaalok05@gmail.com>
2026-08-24 00:16:24 +0530

    Time: 10 ms (16.06%) | Memory: 47.1 MB (26.28%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -6,10 +6,8 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             boolean firstHalf = i < n / 2;
-
-            if (num.charAt(i) == '?') {
-                count += firstHalf ? 1 : -1;
-            } else {
+            if (num.charAt(i) == '?') count += firstHalf ? 1 : -1;
+            else {
                 int digit = num.charAt(i) - '0';
                 diff += firstHalf ? digit : -digit;
             }
